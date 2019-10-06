d265163c0d4d21b0d42c1d6088d51e4d9107d6e4
Anderson Souza <andersom.eugenio@gmail.com>
2019-10-06 12:33:50 -0300

    visibilidade do botao em telas pequenas

--- src/layouts/MyLayout.vue
+++ src/layouts/MyLayout.vue
@@ -1,47 +1,49 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-
     <!-- Header -->
     <grtfood-header>
       Grt Food
       <template v-slot:drawerButton>
-        <q-btn @click="leftDrawerOpen= !leftDrawerOpen" icon="menu" round glossy/>
+        <q-btn class="lt-md" @click="leftDrawerOpen= !leftDrawerOpen" icon="menu" round glossy />
       </template>
     </grtfood-header>
     <!--  -->
-    
+
     <!-- Menu lateral com cardapio -->
-    <grtfood-menu-cardapio :leftDrawerOpen="leftDrawerOpen" @updateParentLeftDrawerOpen="updateParentLeftDrawerOpen"/>
+    <grtfood-menu-cardapio
+      :leftDrawerOpen="leftDrawerOpen"
+      @updateParentLeftDrawerOpen="updateParentLeftDrawerOpen"
+    />
     <!--  -->
 
     <!-- lista com os pedidos -->
     <grtfood-lista-de-pedidos />
     <!--  -->
-
   </q-layout>
 </template>
 
 <script>
-  export default {
-    name: 'MyLayout',
-    data(){
-      return{
-        leftDrawerOpen: true
-      }
-    },
-    components: {
-      'grtfood-header': require('./Header').default,
-      'grtfood-menu-cardapio': require('../components/grtfoodCardapio').default,
-      'grtfood-lista-de-pedidos': require('../components/grtfoodListaDePedidos').default,
-    },
-    methods:{
-      updateParentLeftDrawerOpen(event){
-        this.leftDrawerOpen = event
-      }
+export default {
+  name: "MyLayout",
+  data() {
+    return {
+      leftDrawerOpen: true
+    };
+  },
+  components: {
+    "grtfood-header": require("./Header").default,
+    "grtfood-menu-cardapio": require("../components/grtfoodCardapio").default,
+    "grtfood-lista-de-pedidos": require("../components/grtfoodListaDePedidos")
+      .default
+  },
+  methods: {
+    updateParentLeftDrawerOpen(event) {
+      this.leftDrawerOpen = event;
     }
   }
+};
 </script>
 
 <style lang="stylus">
-  @import url('https://fonts.googleapis.com/css?family=Dosis|Vibes&display=swap');
+@import url('https://fonts.googleapis.com/css?family=Dosis|Vibes&display=swap');
 </style>
