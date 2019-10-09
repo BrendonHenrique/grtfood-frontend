7762ac72d0c89bc43214f6a13869e0b704c40f7c
brendon <bhssilva@inf.ufpel.edu.br>
2019-10-09 10:56:10 -0300

    atualizando layout do menu de opcoes

--- src/components/grtfoodMenuDeOpcoes.vue
+++ src/components/grtfoodMenuDeOpcoes.vue
@@ -1,30 +1,31 @@
 <template>
-  <q-list class="q-pa-md">
-    <q-item clickable v-for="opcao in cardapio.items" :key="opcao">
-      <q-btn class="full-width" dense>
-        <span style="font-family: 'Dosis', sans-serif;font-size:25px;">
-          {{opcao}}
-        </span>
-      </q-btn>
-    </q-item>
-    <template v-for="(valor , chave ) in cardapio.multiplos">
-      <q-item clickable :key="chave">
-        <q-btn class="full-width bg-primary" dense>
-          <span class="text-white" style="font-family: 'Dosis', sans-serif;font-size:25px;">
-            {{chave}}
-          </span>
-        </q-btn>
-      </q-item>
-      <q-item clickable v-for="opcao in valor" :key="opcao">
-        <q-btn class="full-width" dense>
-          <span style="font-family: 'Dosis', sans-serif;font-size:25px;">
-            {{opcao}}
-          </span>
-        </q-btn>
-      </q-item>
-    </template>
-    <!--  -->
-  </q-list>
+    <q-card-section style="flex-grow:1;margin-bottom:4rem;">
+        <q-list class="q-pa-md">
+          <q-item clickable v-for="opcao in cardapio.items" :key="opcao">
+            <q-btn class="full-width" dense>
+              <span style="font-family: 'Dosis', sans-serif;font-size:25px;">
+                {{opcao}}
+              </span>
+            </q-btn>
+          </q-item>
+          <template v-for="(valor , chave ) in cardapio.multiplos">
+            <q-item clickable :key="chave">
+              <q-btn class="full-width bg-primary" dense>
+                <span class="text-white" style="font-family: 'Dosis', sans-serif;font-size:25px;">
+                  {{chave}}
+                </span>
+              </q-btn>
+            </q-item>
+            <q-item clickable v-for="opcao in valor" :key="opcao">
+              <q-btn class="full-width" dense>
+                <span style="font-family: 'Dosis', sans-serif;font-size:25px;">
+                  {{opcao}}
+                </span>
+              </q-btn>
+            </q-item>
+          </template>
+        </q-list>
+    </q-card-section>
 </template>
 
 <script>
